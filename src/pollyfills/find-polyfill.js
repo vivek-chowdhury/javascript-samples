b5ca3468ecb58f6690e7ee77fbc800a393315895
Vivek Chowdhury <vivekchowdhury@gmail.com>
2020-06-23 00:31:43 +0530

    Added polyfill of find

---
+++ src/pollyfills/find-polyfill.js
@@ -0,0 +1,20 @@
+Array.prototype.customFind = function (fn, ...args) {
+    let context = args[0];
+    let result = 0;
+    for (let i = 0; i < this.length; i++) {
+        let match = fn.call(context, this[i], i, this);
+        if (match) {
+            result = this[i];
+            break;
+        }
+    }
+    return result;
+}
+
+function startFindPolyfill() {
+    let mapList = [10, 20, 30, 40, 50];
+    let result = mapList.customFind((x) => {
+        return x > 30;
+    });
+    console.log(result);
+}
